aa72b3e98fddeda85a99a4da330913e3db7bc4a9
Kazi Shakil <116259611+kazishakiil@users.noreply.github.com>
2022-11-07 14:27:48 +0600

    code by shakil,Food Engineering 20,BAU
    calculating age by python

--- birthyeardeattime.py
+++ birthyeardeattime.py
@@ -1,6 +1,6 @@
-import datetime
-x=int(input("Input your Birthyear="))
-today = datetime.date.today()
-y=today.year
-age=y-x
-print(age)
+import datetime #ajker date import er jonne nisi bujhli
+x=int(input("Input your Birthyear=")) #code by shakil fe 20 bau
+today = datetime.date.today() #define kore disi
+y=today.year #eta mane year nilam
+age=y-x #ajker yr theke birth year biyog dilam
+print("your age is",age)
